4568625d4631a487fd6561041f621495f2fa71b9
achu <aswinudayofficial@gmail.com>
2026-03-15 21:05:00 +0530

    defined the  lookup table for microCode

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 RAM={}
 BUS=0b0;
+MicroStep=0;
 REGISTERS={
     "pc":0,
     "mar":0,
@@ -10,26 +11,94 @@ REGISTERS={
     "ir":0,
     "flag":0b10
 }
-StateControls=[
-    "hlt",
-    "pcInc",
-    "pcJ",
-    "pco",
-    "mi",
-    "ri",
-    "ro",
-    "iri",
-    "iro"
-    "rAi",
-    "rAo",
-    "rBi",
-    "aluo",
-    "alusub",
-    "oRegIn",
-    "flagI"
+# LDA=0x1<<0
+# ADD=0x1<<1
+# SUB=0x1<<2
+# STA=0x1<<3
+# LDI=0x1<<4
+# JMP=0x1<<5
+# JC=0x1<<6
+# JZ=0x1<<7
+#
+#################################################
+############Control Lines As bitMasks############
+#################################################
+HLT=0x1<<0
+MI=0x1<<1
+RI=0x1<<2
+RO=0x1<<3
+RI=0x1<<4
+IO=0x1<<5
+II=0x1<<6
+AI=0x1<<7
+AO=0x1<<8
+EO=0x1<<9
+SU=0x1<<10
+BI=0x1<<11
+OI=0x1<<12
+CE=0x1<<13
+CO=0x1<<14
+J=0x1<<15
+FI=0x1<<16
+opCodeDictionary={
+    "LDA":0,
+    "ADD":1,
+    "SUB":2,
+    "STA":3,
+    "LDI":4,
+    "JMP":5,
+    "JC":6,
+    "JZ":7
+}
+
+
+LI=[CO|MI,RO|II|CE] #load instruction
+InstructionLokupPerStep=[
+    {#0b00:zero,carry
+    0x0:LI+[IO|MI,RO|AI,0],
+    0x1:LI+[IO|MI,RO|BI,EO|AI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|FI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|SU|FI],
+    0x3:LI+[IO|MI,RI|AO,0],
+    0x4:LI+[IO|AI,RI|AO,0],
+    0x5:LI+[IO|J,0,0],
+    0x6:LI+[0,0,0],
+    0x7:LI+[0,0,0],
+    },
+    {#0b01:zero,carry
+    0x0:LI+[IO|MI,RO|AI,0],
+    0x1:LI+[IO|MI,RO|BI,EO|AI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|FI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|SU|FI],
+    0x3:LI+[IO|MI,RI|AO,0],
+    0x4:LI+[IO|AI,RI|AO,0],
+    0x5:LI+[IO|J,0,0],
+    0x6:LI+[IO|J,0,0],
+    0x7:LI+[0,0,0],
+    },
+    {#0b10:zero,carry
+    0x0:LI+[IO|MI,RO|AI,0],
+    0x1:LI+[IO|MI,RO|BI,EO|AI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|FI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|SU|FI],
+    0x3:LI+[IO|MI,RI|AO,0],
+    0x4:LI+[IO|AI,RI|AO,0],
+    0x5:LI+[IO|J,0,0],
+    0x6:LI+[0,0,0],
+    0x7:LI+[IO|J,0,0],
+    },
+    {#0b11:zero,carry
+    0x0:LI+[IO|MI,RO|AI,0],
+    0x1:LI+[IO|MI,RO|BI,EO|AI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|FI],
+    0x2:LI+[IO|MI,RO|BI,EO|AI|SU|FI],
+    0x3:LI+[IO|MI,RI|AO,0],
+    0x4:LI+[IO|AI,RI|AO,0],
+    0x5:LI+[IO|J,0,0],
+    0x6:LI+[IO|J,0,0],
+    0x7:LI+[IO|J,0,0],
+    },
 ]
-#todo:encode microcode(rom data) into the instruction lookup
-InstructionLokup={}
 
 
 with open('ram.json', 'r') as file:
